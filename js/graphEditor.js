dc340b3e920fb6141d3a4f72860a847c32a770e6
Chethan-Kumar-k <chethankumark1204@gmail.com>
2026-04-06 19:43:26 +0530

    fourth commit

--- js/graphEditor.js
+++ js/graphEditor.js
@@ -1,6 +1,7 @@
 class GraphEditor {
-    constructor(canvas, graph){
-        this.canvas = canvas;
+    constructor(viewport, graph){
+        this.viewport = viewport;
+        this.canvas = viewport.canvas;
         this.graph = graph;
 
         this.ctx = this.canvas.getContext("2d");
@@ -19,9 +20,10 @@ class GraphEditor {
         this.canvas.addEventListener("contextmenu", (evt) => evt.preventDefault());
         this.canvas.addEventListener("mouseup", () => this.dragging = false);
     }
+
     #handleMouseMove(evt) {
-        this.mouse = new Point(evt.offsetX, evt.offsetY);
-        this.hovered = getNearestPoint(this.mouse, this.graph.points, 10);
+        this.mouse = this.viewport.getMouse(evt, true);
+        this.hovered = getNearestPoint(this.mouse, this.graph.points, 10 * this.viewport.zoom);
         if (this.dragging == true){
             this.selected.x = this.mouse.x;
             this.selected.y = this.mouse.y;
@@ -30,10 +32,10 @@ class GraphEditor {
 
     #handleMouseDown(evt) {
         if (evt.button == 2) { //right click
-            if (this.selected) {
-                this.selected = null;
-            } else if (this.hovered) {
+            if (this.hovered) {
                 this.#removePoint(this.hovered);
+            } else if (this.selected) {
+                this.selected = null;
             }
         }
         if (evt.button == 0) { //left click
@@ -61,6 +63,12 @@ class GraphEditor {
         if(this.selected == point) {
             this.selected = null;
         }
+    }
+
+    dispose() {
+        this.graph.dispose();
+        this.selected = null;
+        this.hovered = null;
     }
 
     display() {
